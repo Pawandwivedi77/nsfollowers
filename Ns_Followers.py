efb7959560cbb97216f7df357472d5a9f8db043a
Pawandwivedi77 <lastpkd@outlook.com>
2024-05-23 14:18:09 +0530

    Update Ns_Followers.py

--- Ns_Followers.py
+++ Ns_Followers.py
@@ -12,7 +12,7 @@ class Ns_Followers:
         self.__AES_NoPadding = AES_CBC_NoPadding()
 
         self.__userid='66995635048' #Enter UserID Here
-        self.__username='pawandwivedipk66' #Enter UserName Here
+        self.__username='meshia.jahani_2286' #Enter UserName Here
         self._session='66995635048%3A0g7F94UcdflNCK%3A13%3AAYdE6nmdDBKzoMHOE2fu4fKGbX6XOK3taAMX8oE0-w' #Enter SessionID Here
 
         self.__Pk=self.__userid.encode()+b'*'+str(random.randint(111,999)).encode()
